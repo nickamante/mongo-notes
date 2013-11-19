47ec666f601a42023e5e983a1baee9a05236c5b9
Nick Amante <nick@nickamante.com>
2013-11-19 14:02:18 -0800

    Added Views/Home/Create.cshtml and updated Views/Home/Index.cshtml

--- MongoNotes/Controllers/HomeController.cs
+++ MongoNotes/Controllers/HomeController.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MongoNotes.DAL;
+using MongoNotes.Models;
 
 namespace MongoNotes.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controller, IDisposable
     {
+        private Dal dal = new Dal();
+        private bool disposed = false;
+
         public ActionResult Index()
         {
+            return View(dal.GetAllNotes());
+        }
+
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Note note)
+        {
+            try
+            {
+                dal.CreateNote(note);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
 
         public ActionResult About()
@@ -26,5 +50,29 @@ namespace MongoNotes.Controllers
 
             return View();
         }
+
+        # region IDisposable
+
+
+        new protected void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        
+        new protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    this.dal.Dispose();
+                }
+            }
+            
+            this.disposed = true;
+        }
+        
+        # endregion
     }
 }
